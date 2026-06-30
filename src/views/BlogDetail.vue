1dfcb31ada339f0717eae0dbac4f57c18ebebce7
hangerzou <zouxinhan@yunxinsoft.cn>
2026-06-30 19:45:04 +0800

    style: 增加内容区域宽度

--- src/views/BlogDetail.vue
+++ src/views/BlogDetail.vue
@@ -260,7 +260,7 @@ const post = computed(() => blogData.value[slug.value])
 
 <style scoped lang="scss">
 .blog-detail {
-  max-width: 780px;
+  max-width: 1000px;
   margin: 0 auto;
   padding: 40px 24px;
 }
